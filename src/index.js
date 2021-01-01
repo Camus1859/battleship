22f2d2a6bbb83eb024085cc20dbdcfc180226e07
Anderson Laventure <Laventurepsu1@gmail.com>
2021-01-01 13:31:03 -0500

    added webpack environment

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+const newyear = 2021;
+
+console.log(`Hello ${newyear} yo`);
+
+const isBabel = !(class {}.toString().indexOf('class ') === 0);
+console.log(isBabel);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  plugins: [new ESLintPlugin()],
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bunde.js',
+    publicPath: 'dist',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
